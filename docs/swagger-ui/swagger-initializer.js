b92d9d4e64c47a4789dd9ac923bed288c93680ba
Robert Klohr <39203582+RobertKlohr@users.noreply.github.com>
2025-05-01 02:33:04 -0700

    Update swagger-initializer.js

--- docs/swagger-ui/swagger-initializer.js
+++ docs/swagger-ui/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://robertklohr.github.io/PowerShellForOnspring/docs/openapi.json",
+    url: "https://robertklohr.github.io/PowerShellForOnspring/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
