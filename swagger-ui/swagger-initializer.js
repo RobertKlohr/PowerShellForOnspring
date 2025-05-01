da53d730aa7631887e2f9b6a586fd824647d48b4
Robert Klohr <39203582+RobertKlohr@users.noreply.github.com>
2025-05-01 01:23:47 -0700

    Update swagger-initializer.js
    disable Try It Out buttons

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -6,6 +6,7 @@ window.onload = function() {
     url: "docs/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
+    supportedSubmitMethods: [],
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
